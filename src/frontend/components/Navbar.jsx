98eb68248f9de953a12a3f359319c9e9057d9f91
Maddie Wright <mwrigh07@student.ubc.ca>
2025-04-05 21:08:06 -0700

    worked on gallery and logo

--- src/frontend/components/Navbar.jsx
+++ src/frontend/components/Navbar.jsx
@@ -5,7 +5,7 @@ import './Navbar.css';
 export default function Navbar() {
   return (
     <header className="navbar">
-      <Link to="/" className="logo">Logo</Link>
+      <Link to="/" className="logo"><img src="./events/ARC'TERYX.png" alt="" /></Link>
       <nav className="nav-links">
         <Link to="/events" className="event">Events</Link>
         <Link to="/groups" className="forum">Forum</Link>
